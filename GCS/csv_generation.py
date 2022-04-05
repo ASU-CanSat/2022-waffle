a05460657ca1ac3d2eeed6f5ec067c9603939cef
emilroy <66329404+emilroy@users.noreply.github.com>
2022-04-04 18:39:53 -0700

    Update csv_generation.py

--- GCS/csv_generation.py
+++ GCS/csv_generation.py
@@ -10,7 +10,7 @@ from csv import writer
 #create the 3 csv files required
 def build():
     #write to container csv file
-    with open('reports/Flight_3226_C.csv', 'w', newline='') as file:
+    with open('reports/Flight_1052_C.csv', 'w', newline='') as file:
         writerC = csv.writer(file)
         #create the csv headers
         writerC.writerow(['TEAM_ID','MISSION_TIME','PACKET_COUNT','PACKET_TYPE','MODE',
@@ -20,18 +20,11 @@ def build():
                         'SP1_PACKET_COUNT','SP2_PACKET_COUNT','CMD_ECHO'])
         file.close()
 
-    #write to payload 1 csv file
+    #write to payload csv file
     with open('reports/Flight_3226_SP1.csv', 'w', newline='') as file:
         writerSP1 = csv.writer(file)
         #create the csv headers
         writerSP1.writerow(['TEAM_ID','MISSION_TIME','PACKET_COUNT','PACKET_TYPE',
-                            'SP_ALTITUDE','SP_TEMP','SP_ROTATION_RATE'])
-        file.close()
-    #write to payload 2 csv file
-    with open('reports/Flight_3226_SP2.csv', 'w', newline='') as file:
-        writerSP2 = csv.writer(file)
-        #create the csv headers
-        writerSP2.writerow(['TEAM_ID','MISSION_TIME','PACKET_COUNT','PACKET_TYPE',
                             'SP_ALTITUDE','SP_TEMP','SP_ROTATION_RATE'])
         file.close()
 
@@ -41,17 +34,12 @@ def append_csv_file(line):
     type = data[3] #to check if it came from container or which payload
     #appends data to the proper csv file
     if type == 'C':
-        with open('reports/Flight_3226_C.csv', 'a', newline='') as file:
+        with open('reports/Flight_1052_C.csv', 'a', newline='') as file:
             writer_object = writer(file)
             writer_object.writerow(data)
             file.close()
     if type == 'S1':
-        with open('reports/Flight_3226_SP1.csv', 'a', newline='') as file:
-            writer_object = writer(file)
-            writer_object.writerow(data)
-            file.close()
-    if type == 'S2':
-        with open('reports/Flight_3226_SP2.csv', 'a', newline='') as file:
+        with open('reports/Flight_1052_SP.csv', 'a', newline='') as file:
             writer_object = writer(file)
             writer_object.writerow(data)
             file.close()
