c95e447c11e19a0b593dfe66f77223866ded8316
Joshua Tenorio <joshliam09@gmail.com>
2022-04-18 18:53:28 -0700

    worked on mpu test

--- mpu9250-test/mpu9250-test.ino
+++ mpu9250-test/mpu9250-test.ino
@@ -1,22 +1,78 @@
 #include "MPU9250.h"
 
 MPU9250 mpu;
+const float MAGNETIC_DECLINATION = 9.8667; //Magnetic declination in Tempe, AZ. Use this site to find for competition https://www.magnetic-declination.com/#
+    
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Wire.begin();
-  delay(2000);
+    Serial.begin(115200);
+    Wire.begin();
+    delay(2000);
 
-  mpu.setup(0x68);
+    if (!mpu.setup(0x68)) {  // change to your own address
+        while (1) {
+            Serial.println("MPU connection failed. Please check the connection.");
+            delay(5000);
+        }
+    }
+  
+    Serial.println("Accel Gyro calibration will start in 5sec.");
+    Serial.println("Please leave the device still on the flat plane.");
+    mpu.verbose(true);
+    delay(5000);
+    mpu.calibrateAccelGyro();
 
+    Serial.println("Mag calibration will start in 5sec.");
+    Serial.println("Please Wave device in a figure eight until done.");
+    delay(5000);
+    mpu.calibrateMag();
+    mpu.setMagneticDeclination(MAGNETIC_DECLINATION);
+
+    print_calibration();
+    mpu.verbose(false);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if(mpu.update()){
-            Serial.print(mpu.getYaw()); Serial.print(", ");
-        Serial.print(mpu.getPitch()); Serial.print(", ");
-        Serial.println(mpu.getRoll());
+            Serial.print("Max x, y, z: ");
+            Serial.print(mpu.getMag(0));
+            Serial.print(" ");
+            Serial.print(mpu.getMag(1));
+            Serial.print(" ");
+            Serial.print(mpu.getMag(2));
+            Serial.print("\n");
   }
 
+}
+
+void print_calibration() {
+    Serial.println("< calibration parameters >");
+    Serial.println("accel bias [g]: ");
+    Serial.print(mpu.getAccBiasX() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getAccBiasY() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getAccBiasZ() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+    Serial.println();
+    Serial.println("gyro bias [deg/s]: ");
+    Serial.print(mpu.getGyroBiasX() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getGyroBiasY() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getGyroBiasZ() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+    Serial.println();
+    Serial.println("mag bias [mG]: ");
+    Serial.print(mpu.getMagBiasX());
+    Serial.print(", ");
+    Serial.print(mpu.getMagBiasY());
+    Serial.print(", ");
+    Serial.print(mpu.getMagBiasZ());
+    Serial.println();
+    Serial.println("mag scale []: ");
+    Serial.print(mpu.getMagScaleX());
+    Serial.print(", ");
+    Serial.print(mpu.getMagScaleY());
+    Serial.print(", ");
+    Serial.print(mpu.getMagScaleZ());
+    Serial.println();
 }
